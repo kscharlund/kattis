1cda1b2986e37c5eef8a875159837770c784dc6b
Karl Schärlund <kalle@scharlund.se>
2021-01-19 18:17:22 +0100

    Add template.cpp

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+int main()
+{
+    cin.sync_with_stdio(false);
+    cin.tie(NULL);
+
+
+
+    return 0;
+}
